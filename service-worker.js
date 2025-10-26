8950366a37444de9a7242bccb67b0f9dd52abdde
Marco-0803 <116687362+Marco-0803@users.noreply.github.com>
2025-10-26 15:56:03 +0100

    Update service worker to version 1 and simplify cache

--- service-worker.js
+++ service-worker.js
@@ -1,44 +1,21 @@
-const CACHE_VERSION = 'v2';
+const CACHE_VERSION = 'v1';
 const CACHE_NAME = `ayto-solver-${CACHE_VERSION}`;
-const APP_SHELL = [
-  '/Ayto/',
-  '/Ayto/index.html',
-  '/Ayto/style.css',
-  '/Ayto/app.js',
-  '/Ayto/manifest.json',
-  '/Ayto/icons/icon-192.png',
-  '/Ayto/icons/icon-512.png',
-  '/Ayto/icons/ayto-logo.jpg'
-];
+const APP_SHELL = ['./','./index.html','./style.css','./app.js','./manifest.json'];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(APP_SHELL)));
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(APP_SHELL)));
   self.skipWaiting();
 });
-
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
+self.addEventListener('activate', e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));
   self.clients.claim();
 });
-
-self.addEventListener('fetch', (e) => {
-  const req = e.request;
-  const html = req.headers.get('accept')?.includes('text/html');
-  if (html) {
-    e.respondWith(
-      fetch(req).catch(() => caches.match('/Ayto/index.html'))
-    );
-    return;
-  }
+self.addEventListener('fetch', e=>{
   e.respondWith(
-    caches.match(req).then(r => r || fetch(req).then(n => {
-      const cp = n.clone();
-      caches.open(CACHE_NAME).then(c => c.put(req, cp));
-      return n;
-    }))
+    caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{
+      const copy=resp.clone();
+      caches.open(CACHE_NAME).then(c=>c.put(e.request,copy));
+      return resp;
+    }).catch(()=>caches.match('./index.html')))
   );
 });
