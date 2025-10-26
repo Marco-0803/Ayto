7dc517871bb0336aba0162adb496887977173e76
Marco-0803 <116687362+Marco-0803@users.noreply.github.com>
2025-10-26 16:25:29 +0100

    Refactor service worker event listeners

--- service-worker.js
+++ service-worker.js
@@ -15,14 +15,10 @@ self.addEventListener('install', e=>{
   e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(APP_SHELL)));
   self.skipWaiting();
 });
-
 self.addEventListener('activate', e=>{
-  e.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))
-  );
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));
   self.clients.claim();
 });
-
 self.addEventListener('fetch', e=>{
   const req=e.request;
   const isHTML=req.headers.get('accept')?.includes('text/html');
